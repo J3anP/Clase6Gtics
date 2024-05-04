cd7f1a6328a02248e39a8847275552bc92073f96
J3anP <a20213733@pucp.edu.pe>
2024-05-03 20:00:06 -0500

    lista2

--- src/main/java/org/example/clase6gtics/controller/HistoryController.java
+++ src/main/java/org/example/clase6gtics/controller/HistoryController.java
@@ -1,4 +1,14 @@
 package org.example.clase6gtics.controller;
 
+import org.example.clase6gtics.repository.HistoryRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/history")
 public class HistoryController {
+    final HistoryRepository historyRepository;
+    public HistoryController( HistoryRepository historyRepository1) {
+        this.historyRepository = historyRepository1;
+    }
 }
--- src/main/java/org/example/clase6gtics/controller/SearchController.java
+++ src/main/java/org/example/clase6gtics/controller/SearchController.java
@@ -1,4 +1,21 @@
 package org.example.clase6gtics.controller;
 
+import org.example.clase6gtics.entity.Employees;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/Search")
 public class SearchController {
+
+    @GetMapping(value = {""})
+    public String listarJobHistory(Model model){
+        List<Employees> empleadosList = historyRepository.listarEmpleados();
+        model.addAttribute("listaEmployee", empleadosList);
+        return "employee/lista";
+    }
 }
